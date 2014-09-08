a14d4c7e15eb3cc0173a01f510c31e274f025fff
boertel <ben@punchtab.com>
2014-09-08 16:27:51 -0700

    background is on the a tag

--- static/js/travelbook.js
+++ static/js/travelbook.js
@@ -1,6 +1,26 @@
 var dayColor,
     content = $('#content')[0];
 
+$(document).keydown(function (e) {
+    // left or j
+    if (e.which === 37 || e.which === 74) {
+    }
+    if (e.which === 39 || e.which === 75) {
+    }
+    // right or k
+});
+
+var hexDigits = new Array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
+
+//Function to convert hex format to a rgb color
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function hex(x) {
+    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+}
 
 function loadDay(number) {
     $('#content').html('');
@@ -8,7 +28,7 @@ function loadDay(number) {
     var name = 'day-' + number,
         bubble = $('.' + name);
 
-    dayColor = bubble.css('background-color');
+    dayColor = bubble.find('a').css('background-color');
 
     $('ul.days li').removeClass('active');
     bubble.addClass('active');
@@ -172,15 +192,3 @@ var Marker = function (args, node) {
         .on('click', function () { console.log('group 1'); }).addTo(map);
     */
 };
-
-var hexDigits = new Array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
-
-//Function to convert hex format to a rgb color
-function rgb2hex(rgb) {
-    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-}
-
-function hex(x) {
-    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-}
