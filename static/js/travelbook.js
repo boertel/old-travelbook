8f7d2db10b42b96995b27592045e64886b053a94
boertel <ben@punchtab.com>
2014-10-09 15:31:16 -0700

    add a small circle when the picture has a marker

--- static/js/travelbook.js
+++ static/js/travelbook.js
@@ -4,8 +4,11 @@ var dayColor,
 var hexDigits = new Array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
 
 //Function to convert hex format to a rgb color
+function extractColor(rgb) {
+    return rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+}
 function rgb2hex(rgb) {
-    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    rgb = extractColor(rgb);
     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 
